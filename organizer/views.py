5448de355834033f81b347b786dd0406c10160dd
Scott Francis <sgf14@netzero.com>
2017-07-21 22:03:54 -0600

    removed non-used imports

---
+++ organizer/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import (get_object_or_404, render)
+from .models import Tag
+
+
+def tag_list(request):
+    return render(
+        request,
+        'organizer/tag_list.html',
+        {'tag_list': Tag.objects.all()}
+    )
+
+
+def tag_detail(request, slug):
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    return render(
+        request,
+        'organizer/tag_detail.html',
+        {'tag': tag}
+    )
+
